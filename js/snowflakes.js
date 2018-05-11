5deb4097bac935e7b2f63b900efafa8c1218c685
kradamsadams <Kradams723@gmail.com>
2018-05-11 11:26:29 -0700

    p5sketch

---
+++ js/snowflakes.js
@@ -0,0 +1,57 @@
+
+let snowflakes = []; // array to hold snowflake objects
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fill(240);
+  noStroke();
+}
+
+function draw() {
+  background('#111363');
+  let t = frameCount / 300; // update time
+
+  // create a random number of snowflakes each frame
+  for (var i = 0; i < random(1); i++) {
+    snowflakes.push(new snowflake()); // append snowflake object
+  }
+
+  // loop through snowflakes with a for..of loop
+  for (let flake of snowflakes) {
+    flake.update(t); // update snowflake position
+    flake.display(); // draw snowflake
+  }
+}
+
+// snowflake class
+function snowflake() {
+  // initialize coordinates
+  this.posX = 0;
+  this.posY = random(-50, 0);
+  this.initialangle = random(0, 2 * PI);
+  this.size = random(1, 20);
+
+  // radius of snowflake spiral
+  // chosen so the snowflakes are uniformly spread out in area
+  this.radius = sqrt(random(pow(width / 2, 2)));
+
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posX = width / 2 + this.radius * sin(angle);
+
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.2);
+
+    // delete snowflake if past end of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
+  };
+
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
+}
